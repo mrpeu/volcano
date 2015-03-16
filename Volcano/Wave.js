1d157af487a71d4c31a4731d03eb0deff00af5a5
mrpeu <gpe.mpro@gmail.com>
2015-03-16 13:33:56 +0100

    wip

--- Volcano/Wave.js
+++ Volcano/Wave.js
@@ -15,6 +15,7 @@ Volcano.Wave = function(conf) {
     this.values = this.conf.values = conf.values;
     this.subd = this.conf.subd = conf.subd || 10;
     this.conf.radius = conf.radius || 1;
+    this.angle = this.conf.angle || Math.PI;
 
     this.points = null;
     this.matPoints = null;
@@ -62,48 +63,16 @@ Volcano.Wave = function(conf) {
 
         scope.scale.set(scope.conf.radius, 1, scope.conf.radius);
 
-        var spline = new THREE.ClosedSplineCurve3(scope.values),
+        var geoPoints = new THREE.Geometry(),
+            geoCurve = new THREE.Geometry(),
+            geoSurface = new THREE.PlaneBufferGeometry(.5, 1, 0.001, scope.values.length * scope.subd, 1, true)
+        ;
 
-        geoPoints = new THREE.Geometry(),
-        geoCurve = new THREE.Geometry(),
-        geoSurface = new THREE.CylinderGeometry(.5, 1, 0.001, scope.values.length * scope.subd, 1, true),
+        updateGeometries( scope.values, scope.angle, geoPoints, geoCurve, geoSurface );
 
-        maxNb = scope.values.length * scope.subd,
-        colPoints = new Array(scope.values.length), colCurve = new Array(maxNb),
-        coeffFullCircle = ((Math.PI * 2) / (scope.values.length * scope.subd)),
-        coeff = 0, position = 0, index = 0;
-
-        for (var i = 0; i < maxNb + 1; i++) {
-
-            index = i / maxNb;
-            position = spline.getPoint(index);
-            coeff = coeffFullCircle * i;
-
-            position.y = Math.max(0, position.y);
-            position.x = Math.cos(coeff) * (1 - (position.y * .1));
-            position.z = Math.sin(coeff) * (1 - (position.y * .1));
-
-            // points
-            if ( (i % scope.subd) == 0) {
-                geoPoints.vertices[~~(i / scope.subd)] = new THREE.Vector3(position.x, position.y +.25, position.z);
-                colPoints[~~(i / scope.subd)] = new THREE.Color(scope.color);
-            }
-
-            // curve
-            geoCurve.vertices[i] = new THREE.Vector3(position.x, position.y, position.z);
-            colCurve[i] = new THREE.Color(scope.color);
-
-            // surface
-            geoSurface.vertices[maxNb - i].x = Math.cos(coeff + Math.PI / 2) * (1 - (position.y * .1));
-            geoSurface.vertices[maxNb - i].z = Math.sin(coeff + Math.PI / 2) * (1 - (position.y * .1));
-            geoSurface.vertices[maxNb - i].y = position.y;
-        }
-
-        geoPoints.colors = colCurve;
         scope.points = new THREE.PointCloud(geoPoints, scope.matPoints);
 
-        geoCurve.colors = colCurve;
-        scope.curve = new THREE.Line(geoCurve, scope.matCurve)
+        scope.curve = new THREE.Line(geoCurve, scope.matCurve);
 
         scope.surface = new THREE.Mesh(geoSurface, scope.matSurface);
         scope.surface.rotation.set(0, Math.PI / 2, 0);
@@ -111,6 +80,46 @@ Volcano.Wave = function(conf) {
         scope.add(scope.points);
         scope.add(scope.curve);
         scope.add(scope.surface);
+    }
+
+    function updateGeometries( values, angle, geoPoints, geoCurve, geoSurface ) {
+        
+        var geoSurfacePosition = geoSurface.attributes.position.array,
+            spline = new THREE.ClosedSplineCurve3(scope.values),
+            maxNb = scope.values.length * scope.subd,
+            coeffFull = ((Math.PI * 2) / (scope.values.length * scope.subd)),
+            coeff = 0, position = 0, index = 0
+        ;
+
+        for (var i = 0; i < maxNb + 1; i++) {
+
+            index = i / maxNb;
+            position = spline.getPoint(index);
+            coeff = coeffFull * i;
+
+            position.y = Math.max(0, position.y);
+            position.x = Math.cos(coeff) * (1 - (position.y * .1));
+            position.z = Math.sin(coeff) * (1 - (position.y * .1));
+
+
+//             // points
+//             if ( (i % scope.subd) == 0) {
+//                 geoPoints.vertices[~~(i / scope.subd)] = new THREE.Vector3(position.x, position.y +.25, position.z);
+//                 geoPoints.colors[~~(i / scope.subd)] = new THREE.Color(scope.color);
+//             }
+
+
+//             // curve
+//             geoCurve.vertices[i] = new THREE.Vector3(position.x, position.y, position.z);
+//             geoCurve.colors[i] = new THREE.Color(scope.color);
+
+
+            // surface
+            geoSurfacePosition[maxNb - i].x = position.x; //Math.cos(coeff + Math.PI / 2) * (1 - (position.y * .1));
+            geoSurfacePosition[maxNb - i].y = position.y;
+            geoSurfacePosition[maxNb - i].z = position.z; //Math.sin(coeff + Math.PI / 2) * (1 - (position.y * .1));
+        }
+
     }
 
     return this;
